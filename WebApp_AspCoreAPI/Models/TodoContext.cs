98e5d7a0276efc3b3884f6170bff25830a2aa091
Joshni Manasa <joshni1995@gmail.com>
2025-01-25 11:24:41 +0530

    First staged commit

---
+++ WebApp_AspCoreAPI/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_AspCoreAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    }
+}
